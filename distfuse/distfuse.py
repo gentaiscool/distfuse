430f7e34c856d6bec86aeb9de643c86e6472df60
gentaiscool <gentaindrawinata@gmail.com>
2024-06-21 21:03:52 -0400

    add prompt for e5 model

--- distfuse/distfuse.py
+++ distfuse/distfuse.py
@@ -58,11 +58,12 @@ class DistFuse():
             scores = model.score_pairs(["I like apple", "I like cats"], ["I like orange", "I like dogs"])
             print(scores)
     """
-    def __init__(self, model_checkpoints:List[List[str]], weights:List[float]=None, dist_measure:str="euclid", openai_token:str=None, cohere_token:str=None, device:str=None):
+    def __init__(self, model_checkpoints:List[List[str]], weights:List[float]=None, instructions:List[str]=None, dist_measure:str="euclid", openai_token:str=None, cohere_token:str=None, device:str=None):
         """
             Args:
                 model_checkpoints (List[str]): a list of model checkpoints and types
                 weights (List[float]): a list of weights
+                instructions (List[str]): a list of instructions
                 dist_measure (str): the distance measure (only accept euclidean, cosine, manhattan, by default: euclidean)
                 openai_token (str): openai token
                 cohere_token (str): cohere token
@@ -70,6 +71,7 @@ class DistFuse():
         """
         self.model_checkpoints = model_checkpoints
         self.models = []
+        self.instructions = []
 
         if dist_measure == "euclidean":
             self.dist_measure = euclidean_distances
@@ -85,6 +87,12 @@ class DistFuse():
             model = EmbeddingModel(model_checkpoint[0], type=model_checkpoint[1], openai_token=openai_token, cohere_token=cohere_token, device=device)
             self.models.append(model)
 
+            if instructions is None:
+                self.instructions.append("")
+            else:
+                assert len(instructions) == len(self.model_checkpoints)
+                self.instructions.append(instructions[i])
+
         if weights is not None:
             self.weights = [1] * len(self.models)
         else:
@@ -92,31 +100,39 @@ class DistFuse():
 
         assert len(self.models) == len(self.weights)
 
-    def score_reference(self, predictions:List[str], references:List[List[str]], aggregate="avg") -> List[float]:
+    def get_detailed_instruct(task_description: str, query: str) -> str:
+        return f'Instruct: {task_description}\nQuery: {query}'
+
+    def score_reference(self, predictions:List[str], references:List[List[str]]) -> List[float]:
         """
             Compute the scores of predictions and references. Each prediction can have a multiple references.
             Args:
                 predictions (List[str]): a list of text sequences (m samples)
                 references (List[List[str]]): a list of list with text sequences (m x r samples) where r is the number of references
-                aggregate (str): a mode to aggregate the scores for multi-reference
             Returns:
                 List[float]: a list of scores (m dimensions)
         """
-        assert aggregate in ["avg", "max"]
         assert len(predictions) > 0 and len(references) > 0
         assert len(predictions) == len(references)
 
         scores = []
-        for model in self.models:
-            embs1 = model.encode(predictions)
-            embs2 = model.encode(references)
+        for model_id, model in zip(range(len(self.models)), self.models):
+            instruction = self.instructions(model_id)
+            if instruction != "":
+                instruction_predictions = []
+                instruction_references = []
+                for prediction, reference in zip(predictions, references):
+                    instruction_predictions.append(self.get_detailed_instruct(instruction, prediction))
+                    instruction_references.append(self.get_detailed_instruct(instruction, reference))
+                embs1 = model.encode(instruction_predictions)
+                embs2 = model.encode(instruction_references)
+            else:
+                embs1 = model.encode(predictions)
+                embs2 = model.encode(references)
         
             for i in range(len(embs1)):
                 reference_scores = self.dist_measure([embs1[i]], [embs2[i]])
-                if aggregate == "avg":
-                    reference_scores = np.mean(np.array(reference_scores), axis=-1)
-                elif aggregate == "max":
-                    reference_scores = np.max(np.array(reference_scores), axis=-1)
+                reference_scores = np.max(np.array(reference_scores), axis=-1)
                 scores.append(reference_scores)
 
         final_scores = scores[0]
@@ -138,9 +154,20 @@ class DistFuse():
         assert len(text_list1) > 0 and len(text_list2) > 0
 
         scores = []
-        for model in self.models:
-            embs1 = model.encode(text_list1)
-            embs2 = model.encode(text_list2)
+        for model_id, model in zip(range(len(self.models)), self.models):
+            instruction = self.instructions(model_id)
+            if instruction != "":
+                instruction_predictions = []
+                instruction_references = []
+                for prediction, reference in zip(text_list1, text_list2):
+                    instruction_predictions.append(self.get_detailed_instruct(instruction, prediction))
+                    instruction_references.append(self.get_detailed_instruct(instruction, reference))
+                embs1 = model.encode(instruction_predictions)
+                embs2 = model.encode(instruction_references)
+            else:
+                embs1 = model.encode(text_list1)
+                embs2 = model.encode(text_list2)
+
             scores.append(self.dist_measure(embs1, embs2))
 
         final_scores = scores[0]
